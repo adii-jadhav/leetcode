569ed66b3c96d0bac3aebd5883f1e346818219f7
Aditya Jadhav <89136092+adii-jadhav@users.noreply.github.com>
2023-09-14 22:38:00 +0530

    Time: 66 ms (68.32%), Space: 17.4 MB (57.33%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        numMap = {}
+        n = len(nums)
+
+        # Build the hash table
+        for i in range(n):
+            numMap[nums[i]] = i
+
+        # Find the complement
+        for i in range(n):
+            complement = target - nums[i]
+            if complement in numMap and numMap[complement] != i:
+                return [i, numMap[complement]]
+
+        return []
