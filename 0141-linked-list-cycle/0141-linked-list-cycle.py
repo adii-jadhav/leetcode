9ec65a1b4510d238616c695e0812b43509099ff1
Aditya Jadhav <89136092+adii-jadhav@users.noreply.github.com>
2023-10-04 18:24:57 +0530

    Time: 54 ms (76.58%), Space: 20.9 MB (7.25%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -1,10 +1,10 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        slow_pointer = head
-        fast_pointer = head
-        while fast_pointer and fast_pointer.next:
-            slow_pointer = slow_pointer.next
-            fast_pointer = fast_pointer.next.next
-            if slow_pointer == fast_pointer:
+        visited_nodes = set()
+        current_node = head
+        while current_node:
+            if current_node in visited_nodes:
                 return True
-        return False
+            visited_nodes.add(current_node)
+            current_node = current_node.next
+        return False                                                                 
