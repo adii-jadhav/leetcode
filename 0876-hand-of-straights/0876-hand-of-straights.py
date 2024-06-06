d204f7cc97264f0741a81b5cc24e85c25d64284f
Aditya Jadhav <89136092+adii-jadhav@users.noreply.github.com>
2024-06-06 22:06:30 +0530

    Time: 168 ms (42.65%), Space: 18.4 MB (63.87%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.py
+++ 0876-hand-of-straights/0876-hand-of-straights.py
@@ -1,25 +1,27 @@
-class Solution(object):
-    def find_successors(self, hand, groupSize, i, n):
-        next_val = hand[i] + 1
-        hand[i] = -1  # Mark as used
-        count = 1
-        i += 1
-        while i < n and count < groupSize:
-            if hand[i] == next_val:
-                next_val = hand[i] + 1
-                hand[i] = -1
-                count += 1
-            i += 1
-        return count == groupSize
-
-    def isNStraightHand(self, hand, groupSize):
-        n = len(hand)
-        if n % groupSize != 0:
-            return False
-        hand.sort()
-        for i in range(n):
-            if hand[i] >= 0:
-                if not self.find_successors(hand, groupSize, i, n):
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        handCounter = defaultdict(int)
+        for val in hand:
+            handCounter[val] += 1
+            
+        allKeys =  list(handCounter.keys())
+        heapq.heapify(allKeys)
+        
+        while allKeys:
+            cur=allKeys[0]
+            
+            for i in range(cur,cur+groupSize):
+                if i not in handCounter:
                     return False
+                handCounter[i]-=1
+                if handCounter[i]==0:
+                    if i != allKeys[0]:
+                        return False
+                    heapq.heappop(allKeys)
+                
+            
         return True
+                    
+                
+        
         
