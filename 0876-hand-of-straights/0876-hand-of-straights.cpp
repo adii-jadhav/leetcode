035e71a394fc6474f5147eada8e99cb21470ce81
Aditya Jadhav <89136092+adii-jadhav@users.noreply.github.com>
2024-06-06 22:09:00 +0530

    Time: 65 ms (40.94%), Space: 30.7 MB (54.6%) - LeetHub

--- 0876-hand-of-straights/0876-hand-of-straights.cpp
+++ 0876-hand-of-straights/0876-hand-of-straights.cpp
@@ -1,28 +1,26 @@
+#include <vector>
+#include <map>
+using namespace std;
+
 class Solution {
 public:
-   bool findSuccessors(vector<int>& hand, int groupSize, int i, int n) {
-        int next = hand[i] + 1;
-        hand[i] = -1;  // Mark as used
-        int count = 1;
-        i += 1;
-        while (i < n && count < groupSize) {
-            if (hand[i] == next) {
-                next = hand[i] + 1;
-                hand[i] = -1;
-                count++;
-            }
-            i++;
-        }
-        return count == groupSize;
-    }
-
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n = hand.size();
-        if (n % groupSize != 0) return false;
-        std::sort(hand.begin(), hand.end());
-        for (int i = 0; i < n; i++) {
-            if (hand[i] >= 0) {
-                if (!findSuccessors(hand, groupSize, i, n)) return false;
+        map<int, int> hsh;
+
+        for(auto val: hand){
+            hsh[val]++;
+        }
+
+        if(n % groupSize != 0) return false;
+
+        while(!hsh.empty()){
+            int minVal = hsh.begin()->first;
+            for(int i = 0; i < groupSize; i++){
+                int currentVal = minVal + i;
+                if(hsh.find(currentVal) == hsh.end()) return false;
+                if(hsh[currentVal] == 1) hsh.erase(currentVal);
+                else hsh[currentVal]--;
             }
         }
         return true;
